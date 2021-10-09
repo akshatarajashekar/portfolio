a187ce1126deb12cbaf962746c9af4fed0c4530b
akshatarajashekar <akshatarajashekar@protonmail.com>
2021-10-09 23:47:41 +0530

    made use of typescript class based component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <PageContainer msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PageContainer from './components/page-container/page-container.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PageContainer
   }
 }
 </script>
---
+++ src/components/page-container/page-container.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+      For a guide and recipes on how to configure / customize this project,<br>
+      check out the
+  </div>
+</template>
+<style>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
+<script>
+import Vue from 'vue';
+
+export default class PageContainer extends Vue{
+}
+</script>
