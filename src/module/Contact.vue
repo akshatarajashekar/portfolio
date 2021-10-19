b72c110ea07a662353f101a8d39bdd1dbf800826
akshatarajashekar <akshatarajashekar@protonmail.com>
2021-10-19 21:14:34 +0530

    added message in contatc page

--- src/module/Contact.vue
+++ src/module/Contact.vue
@@ -1,8 +1,39 @@
 <template>
   <div :class="`${cssPrefix}`">
+    <div  :class="`${cssPrefix}__body`">
+      <div :class="`${cssPrefix}__body_container`">
+        <div :class="`${cssPrefix}__description_container`">
+          <h2  :class="`${cssPrefix}__content`">
+            Leave Message
+          </h2>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
-
+<style lang="scss">
+  .contact {
+    color: white;
+    &__body {
+      
+      padding: 20px 40px;
+      &_container {
+        display: block;
+      }
+    }
+    &__header {
+      display: flex;
+      justify-content: center;
+      font-family: "Open Sans";
+    }
+    &__content {
+      display: flex;
+      justify-content: center;
+      font-family: "Open Sans";
+      font-weight: 300;
+    }
+  }
+</style>
 <script lang="ts">
 import { defineComponent } from 'vue';
 const cssPrefix = 'contact';
