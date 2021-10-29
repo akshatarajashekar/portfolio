4c04af36de0c87765c121e3b36501b1db1f0df71
akshatarajashekar <akshatarajashekar@protonmail.com>
2021-10-29 19:56:45 +0530

    changed scss for scroll bar in dark mode

--- src/shared/scroll-bar.vue
+++ src/shared/scroll-bar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :class="[`${cssPrefix}`]">
+  <div :class="[`${cssPrefix}`,  selectedTheme === 'light' ? `${cssPrefix}__light_theme` : `${cssPrefix}__dark_theme`]">
     <slot></slot>
   </div>
 </template>
@@ -9,28 +9,44 @@
   height: 100%;
   overflow: auto;
 }
-.scroll-bar::-webkit-scrollbar-track {
+.scroll-bar__light_theme::-webkit-scrollbar-track {
   // -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3); // to remove background
   border-radius: 10px;
   background-color: #ffffff;
 }
-.scroll-bar::-webkit-scrollbar {
+.scroll-bar__dark_theme::-webkit-scrollbar-track {
+  border-radius: 10px;
+  background-color: #17223B;
+}
+.scroll-bar__light_theme::-webkit-scrollbar {
   width: 8px;
   background-color: #ffffff;
+}
+.scroll-bar__dark_theme::-webkit-scrollbar {
+  width: 8px;
+  background-color: #17223B;
 }
 .scroll-bar::-webkit-scrollbar-thumb {
   border-radius: 10px;
   -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, .3);
   background-color: #5555555c;
 }
-.scroll-bar::-moz-scrollbar-track {
+.scroll-bar__light_theme::-moz-scrollbar-track {
   // -moz-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
   border-radius: 10px;
   background-color: #ffffff;
 }
-.scroll-bar::-moz-webkit-scrollbar {
+.scroll-bar::-moz-scrollbar-track {
+  border-radius: 10px;
+  background-color: #17223B;
+}
+.scroll-bar__light_theme::-moz-webkit-scrollbar {
   width: 8px;
   background-color: #ffffff;
+}
+.scroll-bar__dark_theme::-moz-webkit-scrollbar {
+  width: 8px;
+  background-color: #17223B;
 }
 .scroll-bar::-moz-webkit-scrollbar-thumb {
   border-radius: 10px;
@@ -41,10 +57,13 @@
 </style>
 <script lang="ts">
 import { defineComponent } from 'vue';
+
+import { mapGetters } from 'vuex';
 const cssPrefix = 'scroll-bar';
 export default defineComponent({
   name: 'ScrollBar',
-  components: {
+  computed: {
+    ...mapGetters('coreStoreModule', ['selectedTheme'])
   },
   data() {
     return {
